bfff043632c5e02924e1361244ae6d2df4956936
Val0007 <valliyappanvelu888@gmail.com>
2026-01-05 19:58:37 +0100

    added only smallcasing for subdomains

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -55,7 +55,7 @@ export class AuthService {
 
   private makeid(length) {
     var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
